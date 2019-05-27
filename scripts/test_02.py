a39e6fb415636db7183a091cd8339cc8bd71218a
zfy19950204 <1184183187@qq.com>
2019-05-27 11:14:11 +0800

    初始化项目

--- scripts/test_02.py
+++ scripts/test_02.py
@@ -10,6 +10,6 @@ class Test_002:
     @allure.step(title="这是放入截图的测试步骤")
     def test_add_png(self):
         print("我是截图")
-        with open("C:\\Users\\86131\\Desktop\\untitled1\\scripts\\rooo.png", "rb") as f:
+        with open("C:\\Users\\deen\\Desktop\\app_08\\image\\abc.png", "rb") as f:
             allure.attach("我是超人{}".format(time.strftime('%Y%m%r_%H%M%S')), f.read(), allure.attach_type.PNG)
         assert True
