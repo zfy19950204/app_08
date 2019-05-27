58125f1809513589a9fa86abf108cf223a3d73e5
zfy19950204 <1184183187@qq.com>
2019-05-27 10:33:13 +0800

    初始化项目

---
+++ test_02.py
@@ -0,0 +1,15 @@
+import allure
+import pytest
+
+class Test_02:
+
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    @allure.step(title="这是一个测试步骤01")
+    def test_001(self):
+        print("我是一个测试方法1")
+        allure.attach("标题", "具体内容")
+        assert True
+
+    def test_add_png(self):
+        with open("\\Users\\deen\\Desktop\\app_po_02\\scripts\\abc.PNG", "rb") as f:
+            allure.attach("截图", f.read(), allure.attach_type.PNG)
