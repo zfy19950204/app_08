2feafa5b27c156e34d513ef7389c77853cd152cc
zfy19950204 <1184183187@qq.com>
2019-05-31 09:56:50 +0800

    初始化项目

--- scripts/test_02.py
+++ scripts/test_02.py
@@ -5,11 +5,10 @@ import pytest
 
 
 class Test_002:
+    def test_001(self):
+        print("我是一个测试方法1")
+        assert True
 
-    @pytest.allure.severity(pytest.allure.severity_level.MINOR)
-    @allure.step(title="这是放入截图的测试步骤")
-    def test_add_png(self):
-        print("我是截图")
-        with open("C:\\Users\\deen\\Desktop\\app_08\\image\\abc.png", "rb") as f:
-            allure.attach("我是超人{}".format(time.strftime('%Y%m%r_%H%M%S')), f.read(), allure.attach_type.PNG)
+    def test_002(self):
+        print("我是一个测试方法2")
         assert True
